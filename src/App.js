27534b3e661bd542a6210d21be242fbee2b400ac
Niket576 <niketchaudhary576@gmail.com>
2025-03-20 19:17:19 +0530

    add all the changes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   return (
     <>
       <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+        <Navbar title="TextTool" mode={mode} toggleMode={toggleMode}/>
         <Alert alert={alert}/>
         <div className="container my-3">
           <Routes>
